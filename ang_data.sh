dab4c6f6e28553a277f6217865e141133a75d067
Jeremy Heyl <heyl@phas.ubc.ca>
2016-06-15 11:11:15 -0700

    angdist plot

--- ang_data.sh
+++ ang_data.sh
@@ -1,1 +1,4 @@
-ctioga2 --name ang_data ang_res.dat
+ctioga2 --name ang_data ang_res.dat	\
+       --xlabel 'Angular Shift [degrees]' \
+	--ylabel 'Pearson Correlation Coefficient $r$' \
+
--- cldist.sh
+++ cldist.sh
@@ -1,2 +1,4 @@
 ctioga2 --yfact 0.001 --name cldist sorted2.lst \
-	--math /xrange -0.2:0.2 '(erf(x/0.06624878354105386/sqrt(2))+1)/2.0*1000'
+	--math /xrange -0.2:0.2 '(erf(x/0.06624878354105386/sqrt(2))+1)/2.0*1000' \
+	--xlabel 'Pearson Correlation Coefficient $r$' \
+        --ylabel 'Fraction of Trials with the Measured $r_m < r$' \
