5a9de12f88c2bfa1c337e0b9718a37cfa44f64f7
Jeremy Heyl <heyl@phas.ubc.ca>
2016-06-15 10:55:59 -0700

    cleantest plot

--- cleantest.sh
+++ cleantest.sh
@@ -1,4 +1,5 @@
 ctioga2 --name cleantest cleantest_sorted.dat \
-	--xlabel "Pearson\'s Correlation Coefficient $r$" \
-        --ylabel 'Fraction of Trials with the Measured $r_m < r$'
+	--xlabel 'Pearson Correlation Coefficient $r$' \
+        --ylabel 'Fraction of Trials with the Measured $r_m < r$' \
+	--math /xrange 0.05:0.55 'erf((x-0.26715063626373625)/sqrt(2)/0.099593036538962398)*0.5+0.5' 
 
