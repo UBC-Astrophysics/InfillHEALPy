1a8390a40c45462573be498ee1d77481b40efd21
Jeremy Heyl <heyl@jonah.phas.ubc.ca>
2016-06-15 16:54:29 -0700

    infill

--- ang_data.sh
+++ ang_data.sh
@@ -1,4 +1,4 @@
 ctioga2 --name ang_data ang_res.dat	\
        --xlabel 'Angular Shift [degrees]' \
-	--ylabel 'Pearson Correlation Coefficient $r$' \
+       --ylabel 'Pearson Correlation Coefficient $r$' \
 
