790f7da4bc314384baf5ee1f33ef8acf706b5b6e
Jeremy Heyl <heyl@jonah.phas.ubc.ca>
2016-06-15 10:49:01 -0700

    cleantest

--- cleantest.sh
+++ cleantest.sh
@@ -1,1 +1,4 @@
-ctioga2 --name cleantest cleantest_sorted.dat 
+ctioga2 --name cleantest cleantest_sorted.dat \
+	--xlabel "Pearson\'s Correlation Coefficient $r$" \
+        --ylabel 'Fraction of Trials with the Measured $r_m < r$'
+
